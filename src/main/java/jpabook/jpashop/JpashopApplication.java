e80d97ceda60230fba0c19167b189fe2de3f9611
kirin765 <asipalseri@gmail.com>
2022-08-15 15:24:32 +0900

    프로젝트 생성

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setName("hello");
+		String name = hello.getName();
+		System.out.println("name = " + name);
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
